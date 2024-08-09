8367193e3a6820e9c837120f4e276b9ae353ce03
bruhGrassi <brunagm88@gmail.com>
2024-08-09 10:54:32 -0300

    chore: create base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,97 @@
+import CurrencyInput from '@/components/CurrencyInput/CurrencyInput';
+import CurrencyChart from '@/components/CurrencyChart/CurrencyChart';
+import CustomButton from './components/CustomButton/CustomButton';
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [primaryAmount, setPrimaryAmount] = useState('');
+  const [secondaryAmount, setSecondaryAmount] = useState('');
+  const [primaryCurrency, setPrimaryCurrency] = useState('US');
+  const [secondaryCurrency, setSecondaryCurrency] = useState('US');
+  const [period, setPeriod] = useState('30');
+
+  const handleAmountChange = (newAmount, field) => {
+    if (field === 'primary') {
+      setPrimaryAmount(newAmount);
+    } else {
+      setSecondaryAmount(newAmount);
+    }
+  };
+
+  const handleCurrencyChange = (newCurrency, field) => {
+    if (field === 'primary') {
+      setPrimaryCurrency(newCurrency);
+    } else {
+      setSecondaryCurrency(newCurrency);
+    }
+  };
+
+  const handlePeriod = (newPeriod) => {
+    setPeriod(newPeriod);
+  };
+
+  const changeCurrency = () => {
+    console.log('change');
+  };
+
+  const currencyData = [
+    { date: '2024-07-02', value: 5.1 },
+    { date: '2024-07-02', value: 2.9 },
+    { date: '2024-07-02', value: 1.1 },
+    { date: '2024-07-02', value: 3.1 },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex items-center gap-4 mb-4">
+        <CurrencyInput
+          amount={primaryAmount}
+          currency={primaryCurrency}
+          onAmountChange={(amount) => handleAmountChange(amount, 'primary')}
+          onCurrencyChange={(currency) =>
+            handleCurrencyChange(currency, 'primary')
+          }
+        />
+
+        <CustomButton icon={true} onClick={changeCurrency} />
+
+        <CurrencyInput
+          amount={secondaryAmount}
+          currency={secondaryCurrency}
+          onAmountChange={(amount) => handleAmountChange(amount, 'secondary')}
+          onCurrencyChange={(currency) =>
+            handleCurrencyChange(currency, 'secondary')
+          }
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="w-100 mb-4">
+        <p className="text-2xl mt-16 mb-4">1 Dólar americano</p>
+        <p className="text-4xl mb-12">5 Reais brasileiros</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="w-100 flex items-center gap-4 mb-4">
+        <CustomButton
+          text="5 Dias"
+          isActive={period === '5'}
+          onClick={() => handlePeriod('5')}
+        />
+        <CustomButton
+          text="1 Mês"
+          isActive={period === '30'}
+          onClick={() => handlePeriod('30')}
+        />
+      </div>
+
+      <section>
+        <CurrencyChart period={period} data={currencyData} />
+      </section>
+
+      <footer className="mt-16">
+        <a href="https://github.com/bruhGrassi" target="_blank">
+          Create by <span>Bruna Grassi</span>
+        </a>
+      </footer>
     </>
   );
 }
